fe31231aa47b56184f3bebea05fcc54e345a3f32
gzq <2025973942qq.com>
2022-12-08 19:26:07 +0800

    第四天

---
+++ 4.浮点型与字符型/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+	float f = 3e-3;//e代表10的幂次
+	printf("输出浮点数 %f\n", f);//%f就是以浮点形式输出对应数据
+	char c = 'a';//定义一个字符型变量，并赋值为'a'
+	printf("%c\n", c);
+	printf("%d\n", c);
+}
